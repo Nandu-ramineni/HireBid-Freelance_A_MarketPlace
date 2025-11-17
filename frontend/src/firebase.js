90466ec808592fa0b984618a367e0c5128bd56c5
Nandu-ramineni <nandurameneni5577@gmail.com>
2025-11-18 03:10:36 +0530

    fix: (env imports)

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBVJAkn8H1iZWLUaaLkOXTaRcOxPLIuTao",
-    authDomain: "freelance-ccb80.firebaseapp.com",
-    projectId: "freelance-ccb80",
-    storageBucket: "freelance-ccb80.appspot.com",
-    messagingSenderId: "222610329903",
-    appId: "1:222610329903:web:58e9852b0c27ec07b2d1da",
-    measurementId: "G-072115QN44"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 
